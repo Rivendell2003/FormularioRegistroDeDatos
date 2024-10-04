29dfded4c9a9baf8ad672d622034a4ef90d52430
Jonny <Rivendell2003@outlook.com>
2024-10-03 22:50:13 -0300

    v1.1

--- app/src/main/java/com/example/formularioregistrodedatosEI6/MainActivity.java
+++ app/src/main/java/com/example/formularioregistrodedatosEI6/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.formularioregistrodedatosEI6;
 
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Patterns; 
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
